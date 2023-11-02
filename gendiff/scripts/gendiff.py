3a262ffb73fb1c201384a00d18aa3caa017d78b6
dmkael <tanmaar@gmail.com>
2023-11-02 17:43:31 +0300

    ADD optional argument FORMAT with key: -f or --format

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,6 +7,7 @@ def main():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument('-f', '--format')
     args = parser.parse_args()
     return args
 
