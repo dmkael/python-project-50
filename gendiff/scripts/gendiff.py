a811affe921d95a3ff4557ee6b265646fa9a7704
dmkael <tanmaar@gmail.com>
2023-11-02 18:08:53 +0300

    MOD change gendiff script to run without func

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,16 +1,13 @@
 import argparse
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
-    )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument('-f', '--format')
-    args = parser.parse_args()
-    return args
+parser = argparse.ArgumentParser(
+    description="Compares two configuration files and shows a difference."
+)
+parser.add_argument("first_file")
+parser.add_argument("second_file")
+parser.add_argument('-f', '--format')
+args = parser.parse_args()
 
-
-if __name__ == "__main__":
-    main()
+file1 = args.first_file
+file2 = args.second_file
