8da529c60d875d3002c23634fcff7d1b284de8da
dmkael <tanmaar@gmail.com>
2023-11-07 17:53:33 +0300

    MOD main gendiff module

---
+++ gendiff/__init__.py
@@ -0,0 +1,6 @@
+from .gendiff import generate_diff
+
+
+__all__ = (
+    "generate_diff",
+)
--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,21 +1,25 @@
 import json
-from gendiff.json_diff import json_diff
+from gendiff.json_diff import get_json_diff
 
 
 def generate_diff(file1, file2):
-    if file1[-5:] == ".json" and file2[-5:] == ".json":
+    if file1.endswith(".json") and file2.endswith(".json"):
         file1 = json.load(open(file1))
         file2 = json.load(open(file2))
-        result = dict_to_str(json_diff(file1, file2))
+        result = dict_to_str(get_json_diff(file1, file2))
         return result
     else:
-        return "other file types under development"
+        return "in development"
 
 
 def dict_to_str(input_dict):
     result_str = "{"
     if input_dict:
         for key in input_dict:
+            if input_dict[key] is True:
+                input_dict[key] = 'true'
+            if input_dict[key] is False:
+                input_dict[key] = 'false'
             result_str += f"\n{key}: {input_dict[key]}"
     result_str = result_str + "\n}"
     return result_str
