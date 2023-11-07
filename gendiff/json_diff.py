c3287a2af8eb87400dee86149ad5b4ca15972651
dmkael <tanmaar@gmail.com>
2023-11-07 17:44:44 +0300

    RF refactored json_diff

--- gendiff/json_diff.py
+++ gendiff/json_diff.py
@@ -2,11 +2,13 @@ def get_json_diff(file1, file2):
     all_keys = sorted(file1 | file2)
     result_dict = {}
     for key in all_keys:
-        if file1.get(key) == file2.get(key):
-            result_dict[f"    {key}"] = file1.get(key)
+        value1 = file1.get(key)
+        value2 = file2.get(key)
+        if value1 == value2:
+            result_dict[f"    {key}"] = value1
         else:
-            if key in file1:
-                result_dict[f"  - {key}"] = file1.get(key)
-            if key in file2:
-                result_dict[f"  + {key}"] = file2.get(key)
+            if value1 is not None:
+                result_dict[f"  - {key}"] = value1
+            if value2 is not None:
+                result_dict[f"  + {key}"] = value2
     return result_dict
