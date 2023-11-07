17fcfe6efa7f3be51e9882a6e34153e6b9787e40
dmkael <tanmaar@gmail.com>
2023-11-07 16:17:32 +0300

    Revert "RF refactored json_diff for less complexity"
    This reverts commit 96dd6d43f9c152194fe7b18f39ed25f61baf40ec.

--- gendiff/json_diff.py
+++ gendiff/json_diff.py
@@ -3,10 +3,10 @@ def get_json_diff(file1, file2):
     result_dict = {}
     for key in all_keys:
         if file1.get(key) == file2.get(key):
-            result_dict[f"{key}"] = file1.get(key)
+            result_dict[f"    {key}"] = file1.get(key)
         else:
             if key in file1:
-                result_dict[f"-{key}"] = file1.get(key)
+                result_dict[f"  - {key}"] = file1.get(key)
             if key in file2:
-                result_dict[f"+{key}"] = file2.get(key)
+                result_dict[f"  + {key}"] = file2.get(key)
     return result_dict
