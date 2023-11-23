0e67128ea1cba4863a20841747eef44915e7c9c1
dmkael <tanmaar@gmail.com>
2023-11-24 00:37:56 +0300

    RF stringify func to less complexity

--- gendiff/diff_formatters/stringify_consts.py
+++ gendiff/diff_formatters/stringify_consts.py
@@ -3,9 +3,8 @@ def format_consts_to_str(diff_dict):
         value = diff_dict[key]
         if isinstance(value, dict):
             format_consts_to_str(value)
-        else:
-            if isinstance(value, bool):
-                diff_dict[key] = str(value).lower()
-            if value is None:
-                diff_dict[key] = 'null'
+        if isinstance(value, bool):
+            diff_dict[key] = str(value).lower()
+        if value is None:
+            diff_dict[key] = 'null'
     return diff_dict
