d2e6e3789b9b66906cd4c0582308dd2aaeb125cf
dmkael <tanmaar@gmail.com>
2023-11-24 00:43:14 +0300

    Revert "MOD stringify remove value var"
    This reverts commit f95971467a0b8faced411c77a67ec6be8ba989f8.

--- gendiff/diff_formatters/stringify_consts.py
+++ gendiff/diff_formatters/stringify_consts.py
@@ -1,9 +1,10 @@
 def format_consts_to_str(diff_dict):
     for key in diff_dict:
-        if isinstance(diff_dict[key], dict):
-            format_consts_to_str(diff_dict[key])
-        if isinstance(diff_dict[key], bool):
-            diff_dict[key] = str(diff_dict[key]).lower()
-        if diff_dict[key] is None:
+        value = diff_dict[key]
+        if isinstance(value, dict):
+            format_consts_to_str(value)
+        if isinstance(value, bool):
+            diff_dict[key] = str(value).lower()
+        if value is None:
             diff_dict[key] = 'null'
     return diff_dict
