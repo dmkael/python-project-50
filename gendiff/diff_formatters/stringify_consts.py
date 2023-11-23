f95971467a0b8faced411c77a67ec6be8ba989f8
dmkael <tanmaar@gmail.com>
2023-11-24 00:41:10 +0300

    MOD stringify remove value var

--- gendiff/diff_formatters/stringify_consts.py
+++ gendiff/diff_formatters/stringify_consts.py
@@ -1,10 +1,9 @@
 def format_consts_to_str(diff_dict):
     for key in diff_dict:
-        value = diff_dict[key]
-        if isinstance(value, dict):
-            format_consts_to_str(value)
-        if isinstance(value, bool):
-            diff_dict[key] = str(value).lower()
-        if value is None:
+        if isinstance(diff_dict[key], dict):
+            format_consts_to_str(diff_dict[key])
+        if isinstance(diff_dict[key], bool):
+            diff_dict[key] = str(diff_dict[key]).lower()
+        if diff_dict[key] is None:
             diff_dict[key] = 'null'
     return diff_dict
