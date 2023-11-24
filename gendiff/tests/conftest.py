6cf5ba7269cfb4a793dc8a40e3ccaab387786644
dmkael <tanmaar@gmail.com>
2023-11-24 20:32:17 +0300

    RF pytest fixtures dir as const

--- gendiff/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-import pytest
-
-
-@pytest.fixture
-def get_fixtures_dir():
-    current_dir = Path(__file__).parent
-    fixtures_dir = current_dir / "fixtures"
-    return fixtures_dir
--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,13 +1,17 @@
 from gendiff.gendiff import generate_diff
+from pathlib import Path
 import json
 
 
-def test_gendiff_json_to_stylish(get_fixtures_dir):
-    file1 = get_fixtures_dir / "file1.json"
-    file2 = get_fixtures_dir / "file2.json"
-    file3 = get_fixtures_dir / "file3.json"
-    result_file1 = get_fixtures_dir / "result1_stylish.txt"
-    result_file2 = get_fixtures_dir / "result2_stylish.txt"
+FIXTURES_DIR = Path(__file__).parent / "fixtures"
+
+
+def test_gendiff_json_to_stylish():
+    file1 = FIXTURES_DIR / "file1.json"
+    file2 = FIXTURES_DIR / "file2.json"
+    file3 = FIXTURES_DIR / "file3.json"
+    result_file1 = FIXTURES_DIR / "result1_stylish.txt"
+    result_file2 = FIXTURES_DIR / "result2_stylish.txt"
     with open(result_file1, "r") as result1:
         result1 = result1.read()
         assert generate_diff(file1, file2, 'stylish') == result1
@@ -16,21 +20,21 @@ def test_gendiff_json_to_stylish(get_fixtures_dir):
         assert generate_diff(file1, file3, 'stylish') == result2
 
 
-def test_gendiff_yaml_to_stylish(get_fixtures_dir):
-    file1 = get_fixtures_dir / "example1.yaml"
-    file2 = get_fixtures_dir / "example2.yml"
-    result_file1 = get_fixtures_dir / "result1_stylish.txt"
+def test_gendiff_yaml_to_stylish():
+    file1 = FIXTURES_DIR / "example1.yaml"
+    file2 = FIXTURES_DIR / "example2.yml"
+    result_file1 = FIXTURES_DIR / "result1_stylish.txt"
     with open(result_file1, "r") as result1:
         result = result1.read()
         assert generate_diff(file1, file2, 'stylish') == result
 
 
-def test_gendiff_json_to_plain(get_fixtures_dir):
-    file1 = get_fixtures_dir / "file1.json"
-    file2 = get_fixtures_dir / "file2.json"
-    file3 = get_fixtures_dir / "file3.json"
-    result_file1 = get_fixtures_dir / "result1_plain.txt"
-    result_file2 = get_fixtures_dir / "result2_plain.txt"
+def test_gendiff_json_to_plain():
+    file1 = FIXTURES_DIR / "file1.json"
+    file2 = FIXTURES_DIR / "file2.json"
+    file3 = FIXTURES_DIR / "file3.json"
+    result_file1 = FIXTURES_DIR / "result1_plain.txt"
+    result_file2 = FIXTURES_DIR / "result2_plain.txt"
     with open(result_file1, "r") as result1:
         result1 = result1.read()
         assert generate_diff(file1, file2, 'plain') == result1
@@ -39,27 +43,27 @@ def test_gendiff_json_to_plain(get_fixtures_dir):
         assert generate_diff(file1, file3, 'plain') == result2
 
 
-def test_gendiff_yaml_to_plain(get_fixtures_dir):
-    file1 = get_fixtures_dir / "example1.yaml"
-    file2 = get_fixtures_dir / "example2.yml"
-    result_file1 = get_fixtures_dir / "result1_plain.txt"
+def test_gendiff_yaml_to_plain():
+    file1 = FIXTURES_DIR / "example1.yaml"
+    file2 = FIXTURES_DIR / "example2.yml"
+    result_file1 = FIXTURES_DIR / "result1_plain.txt"
     with open(result_file1, "r") as result1:
         result = result1.read()
         assert generate_diff(file1, file2, 'plain') == result
 
 
-def test_gendiff_json_to_json(get_fixtures_dir):
-    file1 = get_fixtures_dir / "file1.json"
-    file2 = get_fixtures_dir / "file2.json"
-    result_file1 = get_fixtures_dir / "result1.json"
+def test_gendiff_json_to_json():
+    file1 = FIXTURES_DIR / "file1.json"
+    file2 = FIXTURES_DIR / "file2.json"
+    result_file1 = FIXTURES_DIR / "result1.json"
     diff = generate_diff(file1, file2, 'json')
     with open(result_file1) as result_json:
         result = json.load(result_json)
         assert json.loads(diff) == result
 
 
-def test_file_loader_for_other_files(get_fixtures_dir):
-    file1 = get_fixtures_dir / "file1.json"
-    file2 = get_fixtures_dir / "file4.doc"
+def test_file_loader_for_other_files():
+    file1 = FIXTURES_DIR / "file1.json"
+    file2 = FIXTURES_DIR / "file4.doc"
     diff = generate_diff(file1, file2)
     assert diff == "'.doc' is not supported file type"
