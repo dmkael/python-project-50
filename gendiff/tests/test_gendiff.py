a161fecd220e7b944166fb0bb54cd399581ff0bf
dmkael <tanmaar@gmail.com>
2023-12-02 19:52:27 +0300

    RF refactored tests

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -1,71 +1,62 @@
-import pytest
-
 from gendiff.gendiff import generate_diff
 from pathlib import Path
 import json
+import pytest
 
 
 FIXTURES_DIR = Path(__file__).parent / "fixtures"
+json1 = FIXTURES_DIR / "file1.json"
+json2 = FIXTURES_DIR / "file2.json"
+json3 = FIXTURES_DIR / "file3.json"
+yaml1 = FIXTURES_DIR / "example1.yaml"
+yaml2 = FIXTURES_DIR / "example2.yml"
+doc1 = FIXTURES_DIR / "file4.doc"
+result_stylish1 = FIXTURES_DIR / "result1_stylish.txt"
+result_stylish2 = FIXTURES_DIR / "result2_stylish.txt"
+result_plain1 = FIXTURES_DIR / "result1_plain.txt"
+result_plain2 = FIXTURES_DIR / "result2_plain.txt"
+result_json1 = FIXTURES_DIR / "result1.json"
 
 
-def test_gendiff_json_to_stylish():
-    file1 = FIXTURES_DIR / "file1.json"
-    file2 = FIXTURES_DIR / "file2.json"
-    file3 = FIXTURES_DIR / "file3.json"
-    result_file1 = FIXTURES_DIR / "result1_stylish.txt"
-    result_file2 = FIXTURES_DIR / "result2_stylish.txt"
-    with open(result_file1, "r") as result1:
-        result1 = result1.read()
-        assert generate_diff(file1, file2, 'stylish') == result1
-    with open(result_file2, 'r') as result2:
-        result2 = result2.read()
-        assert generate_diff(file1, file3, 'stylish') == result2
+test_cases_stylish = [
+    ((json1, json2), result_stylish1),
+    ((json1, json3), result_stylish2),
+    ((yaml1, yaml2), result_stylish1)
+]
+
+test_cases_plain = [
+    ((json1, json2), result_plain1),
+    ((json1, json3), result_plain2),
+    ((yaml1, yaml2), result_plain1)
+]
+
+test_cases_json = [
+    ((json1, json2), result_json1)
+]
 
 
-def test_gendiff_yaml_to_stylish():
-    file1 = FIXTURES_DIR / "example1.yaml"
-    file2 = FIXTURES_DIR / "example2.yml"
-    result_file1 = FIXTURES_DIR / "result1_stylish.txt"
-    with open(result_file1, "r") as result1:
-        result = result1.read()
-        assert generate_diff(file1, file2, 'stylish') == result
+@pytest.mark.parametrize("files, expected", test_cases_stylish)
+def test_gendiff_stylish(files, expected):
+    with open(expected, 'r') as result:
+        assert generate_diff(*files) == result.read()
 
 
-def test_gendiff_json_to_plain():
-    file1 = FIXTURES_DIR / "file1.json"
-    file2 = FIXTURES_DIR / "file2.json"
-    file3 = FIXTURES_DIR / "file3.json"
-    result_file1 = FIXTURES_DIR / "result1_plain.txt"
-    result_file2 = FIXTURES_DIR / "result2_plain.txt"
-    with open(result_file1, "r") as result1:
-        result1 = result1.read()
-        assert generate_diff(file1, file2, 'plain') == result1
-    with open(result_file2, 'r') as result2:
-        result2 = result2.read()
-        assert generate_diff(file1, file3, 'plain') == result2
+@pytest.mark.parametrize("files, expected", test_cases_plain)
+def test_gendiff_plain(files, expected):
+    with open(expected, 'r') as result:
+        assert generate_diff(*files, "plain") == result.read()
 
 
-def test_gendiff_yaml_to_plain():
-    file1 = FIXTURES_DIR / "example1.yaml"
-    file2 = FIXTURES_DIR / "example2.yml"
-    result_file1 = FIXTURES_DIR / "result1_plain.txt"
-    with open(result_file1, "r") as result1:
-        result = result1.read()
-        assert generate_diff(file1, file2, 'plain') == result
+@pytest.mark.parametrize("files, expected", test_cases_json)
+def test_gendiff_json(files, expected):
+    with open(expected, 'r') as data:
+        result = data.read()
+        assert json.loads(generate_diff(*files, "json")) == json.loads(result)
 
 
-def test_gendiff_json_to_json():
-    file1 = FIXTURES_DIR / "file1.json"
-    file2 = FIXTURES_DIR / "file2.json"
-    result_file1 = FIXTURES_DIR / "result1.json"
-    diff = generate_diff(file1, file2, 'json')
-    with open(result_file1) as result_json:
-        result = json.load(result_json)
-        assert json.loads(diff) == result
-
-
-def test_file_loader_for_other_files():
-    file1 = FIXTURES_DIR / "file1.json"
-    file2 = FIXTURES_DIR / "file4.doc"
-    with pytest.raises(ValueError):
-        generate_diff(file1, file2)
+def test_gendiff_exceptions():
+    with pytest.raises(Exception) as error_message:
+        generate_diff(json1, doc1)
+        assert str(error_message) == 'Unsupported file type'
+        generate_diff(json1, json2, "something")
+        assert str(error_message) == 'Wrong formatter'
