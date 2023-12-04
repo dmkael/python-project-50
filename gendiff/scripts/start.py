6e847be9160d81830771cf33a58c8967a905d6ae
dmkael <tanmaar@gmail.com>
2023-12-04 15:38:46 +0300

    MOD start script argparse help behavior

--- gendiff/scripts/start.py
+++ gendiff/scripts/start.py
@@ -9,11 +9,16 @@ def main():
     # Закомментируйте sys.tracebacklimit, чтобы отключить подавление.
     sys.tracebacklimit = 0
     parser = argparse.ArgumentParser(
-        description="Compares two configuration files and shows a difference."
+        description="Compares two configuration files and shows a difference.",
+        usage='%(prog)s [options] <first_file> <second_file>'
     )
-    parser.add_argument("first_file")
-    parser.add_argument("second_file")
-    parser.add_argument('-f', '--format', default='stylish')
+    parser.add_argument("<first_file>", help=argparse.SUPPRESS)
+    parser.add_argument("<second_file>", help=argparse.SUPPRESS)
+    parser.add_argument('-f', '--format',
+                        metavar='<style>',
+                        default='stylish',
+                        choices=['stylish', 'plain', 'json'],
+                        help='format with style: %(choices)s')
     args = parser.parse_args()
     diff = generate_diff(args.first_file, args.second_file, args.format)
     if diff:
