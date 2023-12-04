def04e617fccab45c2d89bb3af268bc16020465b
dmkael <tanmaar@gmail.com>
2023-12-04 12:33:18 +0300

    RF json formatter

--- gendiff/diff_formatters/to_json.py
+++ gendiff/diff_formatters/to_json.py
@@ -2,17 +2,17 @@ import json
 from gendiff.diff_formatters.value_check import is_touched_value
 
 
+def prebuild_clean_diff(data):
+    result = {}
+    for key in data:
+        value = data.get(key)
+        if is_touched_value(value):
+            value.pop('d_key')
+            result[key] = value
+        else:
+            result[key] = prebuild_clean_diff(value)
+    return result
+
+
 def make_json(diff):
-
-    def walk(data):
-        result = {}
-        for key in data:
-            value = data.get(key)
-            if is_touched_value(value):
-                value.pop('d_key')
-                result[key] = value
-            else:
-                result[key] = walk(value)
-        return result
-
-    return json.dumps(walk(diff))
+    return json.dumps(prebuild_clean_diff(diff))
