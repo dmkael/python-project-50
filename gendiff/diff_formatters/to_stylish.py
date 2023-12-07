96d66fcb9460873f1d14e8b8946e62e9be6efd8e
dmkael <tanmaar@gmail.com>
2023-12-07 16:19:58 +0300

    Revert "RF stylish formatter"
    This reverts commit 766605ebafbcddec9d9aeaf39ff571209cd300a1.

--- gendiff/diff_formatters/to_stylish.py
+++ gendiff/diff_formatters/to_stylish.py
@@ -7,56 +7,52 @@ CONST_TRANSFORM_PATTERN = {
 }
 
 
-def finalize_value(value, depth, indent):
+def transform_const(value):
+    value = CONST_TRANSFORM_PATTERN.get(str(value), value)
+    return value
+
+
+def finalize_value(value, depth, placer):
     if not isinstance(value, dict):
-        value = CONST_TRANSFORM_PATTERN.get(str(value), value)
+        value = transform_const(value)
         return value
     result = ["{"]
     for key, inner_value in value.items():
-        inner_value = finalize_value(inner_value, depth + 1, indent)
-        result.append(f"{indent * (depth + 1)}{key}: {inner_value}")
-    result.append(f"{indent * depth}" + "}")
+        inner_value = finalize_value(inner_value, depth + 1, placer)
+        result.append(f"{placer * (depth + 1)}{key}: {inner_value}")
+    result.append(f"{placer * depth}" + "}")
     return "\n".join(result)
 
 
-def make_stylish_value(status, indent, key, values):
-    value1, value2 = values
-    stylish_value = None
-    if status == "added":
-        stylish_value = f"{indent}+ {key}: {value1}"
-    elif status == "removed":
-        stylish_value = f"{indent}- {key}: {value1}"
-    elif status == "same":
-        stylish_value = f"{indent}  {key}: {value1}"
-    elif status == "modified":
-        stylish_part1 = f"{indent}- {key}: {value1}\n"
-        stylish_part2 = f"{indent}+ {key}: {value2}"
-        stylish_value = stylish_part1 + stylish_part2
-    return stylish_value
-
-
-def stylize_diff(diff, depth, indent=" ", indent_count=4):
+def stylize_diff(diff, depth, placer=" ", placer_count=4):
+    depth = depth + 1
+    normal_placer = placer * placer_count
+    mod_placer = placer * (placer_count * depth - 2)
     result = ["{"]
-    current_depth = depth + 1
-    normal_indent = indent * indent_count
-    mod_indent = indent * (indent_count * current_depth - 2)
-    closing_indent = normal_indent * (current_depth - 1)
-
     for key, diff_value in diff.items():
-        value = diff_value.get("value", UNIQUE_KEY)
-        value2 = diff_value.get("new_value", UNIQUE_KEY)
+        inner_value = diff_value["value"]
         status = diff_value[UNIQUE_KEY]
-        if status == "nested":
-            result.append(f"{normal_indent * current_depth}{key}: "
-                          + stylize_diff(value, current_depth))
-        else:
-            value = finalize_value(value, current_depth, normal_indent)
-            if value2 != UNIQUE_KEY:
-                value2 = finalize_value(value2, current_depth, normal_indent)
-            values = value, value2
-            result.append(make_stylish_value(status, mod_indent, key, values))
-
-    result.append(f"{closing_indent}" + "}")
+        if status != "nested":
+            inner_value = finalize_value(inner_value, depth, normal_placer)
+        match status:
+            case "nested":
+                result.append(f"{normal_placer * depth}{key}: "
+                              + stylize_diff(inner_value, depth))
+            case "added":
+                result.append(f"{mod_placer}+ {key}: {inner_value}")
+            case "removed":
+                result.append(f"{mod_placer}- {key}: {inner_value}")
+            case "same":
+                result.append(f"{mod_placer}  {key}: {inner_value}")
+            case "modified":
+                inner_value2 = diff_value["new_value"]
+                inner_value2 = finalize_value(
+                    inner_value2, depth, normal_placer
+                )
+                result.append(f"{mod_placer}- {key}: {inner_value}")
+                result.append(f"{mod_placer}+ {key}: {inner_value2}")
+    close_placer = normal_placer * (depth - 1)
+    result.append(f"{close_placer}" + "}")
     return "\n".join(result)
 
 
