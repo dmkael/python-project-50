c6f2d45651ee7fa82610df0ff1a9ba003e06eb0d
dmkael <tanmaar@gmail.com>
2023-12-13 13:30:14 +0300

    RF stylish formatter

--- gendiff/diff_formatters/to_stylish.py
+++ gendiff/diff_formatters/to_stylish.py
@@ -21,33 +21,35 @@ def stylize_diff(diff, depth, indent=" ", indent_count=4):
     normal_indent = indent * indent_count
     style_indent = indent * (indent_count * current_depth - 2)
     closing_indent = normal_indent * (current_depth - 1)
+
     style_parts = ["{"]
     for key, diff_value in diff.items():
         inner_value = diff_value["value"]
         status = diff_value["value_status"]
         if not is_valid_status(status):
             raise ValueError('Invalid status value')
-        if status != "nested":
+        if status == "nested":
+            style_parts.append(f"{normal_indent * current_depth}{key}: "
+                               + stylize_diff(inner_value, current_depth))
+        else:
             inner_value = finalize_value(
                 inner_value, current_depth, normal_indent
             )
-        match status:
-            case "nested":
-                style_parts.append(f"{normal_indent * current_depth}{key}: "
-                                   + stylize_diff(inner_value, current_depth))
-            case "added":
-                style_parts.append(f"{style_indent}+ {key}: {inner_value}")
-            case "removed":
-                style_parts.append(f"{style_indent}- {key}: {inner_value}")
-            case "same":
-                style_parts.append(f"{style_indent}  {key}: {inner_value}")
-            case "modified":
-                inner_value2 = diff_value["new_value"]
-                inner_value2 = finalize_value(
-                    inner_value2, current_depth, normal_indent
-                )
-                style_parts.append(f"{style_indent}- {key}: {inner_value}")
-                style_parts.append(f"{style_indent}+ {key}: {inner_value2}")
+            match status:
+                case "added":
+                    style_parts.append(f"{style_indent}+ {key}: {inner_value}")
+                case "removed":
+                    style_parts.append(f"{style_indent}- {key}: {inner_value}")
+                case "same":
+                    style_parts.append(f"{style_indent}  {key}: {inner_value}")
+                case "modified":
+                    inner_value2 = diff_value["new_value"]
+                    inner_value2 = finalize_value(
+                        inner_value2, current_depth, normal_indent
+                    )
+                    style_parts.append(f"{style_indent}- {key}: {inner_value}")
+                    style_parts.append(f"{style_indent}+ {key}: {inner_value2}")
+
     style_parts.append(f"{closing_indent}" + "}")
     return "\n".join(style_parts)
 
